6314bbc4f8cb4338e7b4a2eb229cfe6f56f217c7
javaxd123 <marcinwarzybok@outlook.com>
2018-07-22 19:27:04 +0200

    root

--- src/app/app.js
+++ src/app/app.js
@@ -5,28 +5,18 @@ import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router';
 
 // Action creators and helpers
-import { establishCurrentUser } from '../modules/auth';
 import { isServer } from '../store';
-
-import Header from './header';
 import Routes from './routes';
 
 import './app.css';
 
 class App extends Component {
   componentWillMount() {
-    if (!isServer) {
-      this.props.establishCurrentUser();
-    }
   }
 
   render() {
     return (
       <div id="app">
-        <Header
-          isAuthenticated={this.props.isAuthenticated}
-          current={this.props.location.pathname}
-        />
         <div id="content">
           <Routes />
         </div>
@@ -35,16 +25,10 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
-});
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ establishCurrentUser }, dispatch);
+  bindActionCreators({}, dispatch);
 
 export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(App)
+  App
 );
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,5 @@
 import { combineReducers } from 'redux';
 
-import auth from './auth';
-import profile from './profile';
-
 export default combineReducers({
-  auth,
-  profile
+  
 });
